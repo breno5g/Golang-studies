4e457db9af0d2820e701595d955fd628d13b8493
Breno Santos <brenosantos145@gmail.com>
2022-05-30 12:00:46 -0300

    :sparkles: Tests improvement
    Criando mais testes

--- ola/ola.go
+++ ola/ola.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt" // Módulo nativo do go
 
+const espanhol = "espanhol"
+const frances = "francês"
 const prefixoOlaPortugues = "Olá, " // const padrão
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(name string) string {
+func Ola(name string, idioma string) string {
 	// Declaração de função com tipagem de parametro e retorno
 	if name == "" {
 		name = "Mundo"
 	}
+
+	if idioma == espanhol {
+		return prefixoOlaEspanhol + name
+	}
+
+	if idioma == frances {
+		return prefixoOlaFrances + name
+	}
+
 	return prefixoOlaPortugues + name // Concatenação basica
 }
 
 func main() { // Função principal que o go chama ao executar o código
-	fmt.Println(Ola("Breno")) // Print do retorno da outra função
+	fmt.Println(Ola("Breno", "espanhol")) // Print do retorno da outra função
 }
--- ola/ola_test.go
+++ ola/ola_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestOla(t *testing.T) {
 	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
@@ -11,14 +13,26 @@ func TestOla(t *testing.T) {
 	}
 
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Chris")
+		resultado := Ola("Chris", "")
 		esperado := "Olá, Chris"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em francês", func(t *testing.T) {
+		resultado := Ola("Breno", "francês")
+		esperado := "Bonjour, Breno"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 }
