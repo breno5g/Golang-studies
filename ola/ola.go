4b9cf37101782e7b3eae7fda1983c604f0b20533
Breno Santos <brenosantos145@gmail.com>
2022-05-30 11:50:09 -0300

    :sparkles: Tests improvement
    Melhorando os testes com a utilização de subtestes e uma função de apoio

--- ola/ola.go
+++ ola/ola.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import "fmt" // Módulo nativo do go
+
+const prefixoOlaPortugues = "Olá, " // const padrão
 
 func Ola(name string) string {
-	return "Olá, " + name
+	// Declaração de função com tipagem de parametro e retorno
+	if name == "" {
+		name = "Mundo"
+	}
+	return prefixoOlaPortugues + name // Concatenação basica
 }
 
-func main() {
-	fmt.Println(Ola("Breno"))
+func main() { // Função principal que o go chama ao executar o código
+	fmt.Println(Ola("Breno")) // Print do retorno da outra função
 }
