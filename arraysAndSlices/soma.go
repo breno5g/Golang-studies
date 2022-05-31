b93e4ad51de27b116a48909e62189fdd267b42d8
Breno Santos <brenosantos145@gmail.com>
2022-05-31 10:21:57 -0300

    :recycle: Refactory to use range
    Range === "for of"

--- arraysAndSlices/soma.go
+++ arraysAndSlices/soma.go
@@ -1,9 +1,18 @@
 package main
 
+// func Soma(numeros [5]int) int {
+// 	sum := 0
+// 	for i := 0; i < 5; i++ {
+// 		sum += numeros[i]
+// 	}
+// 	return sum
+// }
+
 func Soma(numeros [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numeros[i]
+	// Range é tipo a farofa, primeiro parametro é o indice e o segundo o valor
+	for _, numero := range numeros {
+		sum += numero
 	}
 	return sum
 }
