46f390d26b7a29f500e1cc0abc6c26b739af1dff
Breno Santos <brenosantos145@gmail.com>
2022-05-31 10:29:15 -0300

    :recycle: Refactor the test for reduce redundance
    comentando um dos testes para remover a redundancia

--- arraysAndSlices/soma_test.go
+++ arraysAndSlices/soma_test.go
@@ -4,19 +4,19 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	t.Run("coleção de 5 números", func(t *testing.T) {
-		// Array feião mas saudavel
-		// numeros := [5]int{1, 2, 3, 4, 5}
-		numeros := []int{1, 2, 3, 4, 5}
+	// t.Run("coleção de 5 números", func(t *testing.T) {
+	// 	// Array feião mas saudavel e com numero maximo de 5
+	// 	numeros := [5]int{1, 2, 3, 4, 5}
 
-		resultado := Soma(numeros)
-		esperado := 15
+	// 	resultado := Soma(numeros)
+	// 	esperado := 15
 
-		if resultado != esperado {
-			t.Errorf("resultado %d, want %d, dado %v", resultado, esperado, numeros)
-		}
-	})
+	// 	if resultado != esperado {
+	// 		t.Errorf("resultado %d, want %d, dado %v", resultado, esperado, numeros)
+	// 	}
+	// })
 
+	// go test -cover // cobertura do código
 	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
 		// se tu não passar um valor no array, ele vira um slice... que é um array grandão
 		numeros := []int{1, 2, 3}
