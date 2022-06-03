4d97ef5251e710e3d4344be7be5d4dcb1c1a43de
Breno Santos <brenosantos145@gmail.com>
2022-06-03 12:02:30 -0300

    :sparkles: Magic interface
    Isso é real muito peculiar

--- estruturas-metodos-interfaces/perimetro_test.go
+++ estruturas-metodos-interfaces/perimetro_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimetro(t *testing.T) {
 	retangulo := Retangulo{10.0, 10.0}
@@ -12,24 +14,49 @@ func TestPerimetro(t *testing.T) {
 	}
 }
 
+// func TestArea(t *testing.T) {
+// 	t.Run("retângulos", func(t *testing.T) {
+// 		retangulo := Retangulo{12.0, 6.0}
+// 		resultado := retangulo.Area()
+// 		esperado := 72.0
+
+// 		if resultado != esperado {
+// 			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+// 		}
+// 	})
+
+// 	t.Run("círculos", func(t *testing.T) {
+// 		circulo := Circulo{10}
+// 		resultado := circulo.Area()
+// 		esperado := 314.1592653589793
+
+// 		if resultado != esperado {
+// 			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+// 		}
+// 	})
+// }
+
+type Forma interface {
+	Area() float64
+}
+
 func TestArea(t *testing.T) {
-	t.Run("retângulos", func(t *testing.T) {
-		retangulo := Retangulo{12.0, 6.0}
-		resultado := retangulo.Area()
-		esperado := 72.0
+	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
+		t.Helper()
+		resultado := forma.Area()
 
 		if resultado != esperado {
 			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
 		}
+	}
+
+	t.Run("retângulos", func(t *testing.T) {
+		retangulo := Retangulo{12.0, 6.0}
+		verificaArea(t, retangulo, 72.0)
 	})
 
 	t.Run("círculos", func(t *testing.T) {
 		circulo := Circulo{10}
-		resultado := circulo.Area()
-		esperado := 314.1592653589793
-
-		if resultado != esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-		}
+		verificaArea(t, circulo, 314.1592653589793)
 	})
 }
