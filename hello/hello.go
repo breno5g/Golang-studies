8554b6faa59e7d9fda4d5d36b3e573cdfce6d412
Breno Santos <brenosantos145@gmail.com>
2022-10-31 09:58:39 -0300

    :sparkles: add new languages suport and tests

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,25 @@ package hello
 import "fmt"
 
 const prefixInEnglish = "Hello, "
+const prefixInPortuguese = "Olá, "
+const prefixInFrench = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return prefixInEnglish + name
+
+	prefix := prefixInEnglish
+
+	switch language {
+	case "pt-br":
+		prefix = prefixInPortuguese
+	case "fr":
+		prefix = prefixInFrench
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Breno"))
+	fmt.Println(Hello("Breno", "pt-br"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -15,7 +15,7 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello with parameter", func(t *testing.T) {
-		result := Hello("Breno")
+		result := Hello("Breno", "")
 		expected := "Hello, Breno"
 
 		// t param is required because helper function doesn't have access to to t
@@ -23,9 +23,23 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("Return default response with no parameter", func(t *testing.T) {
-		result := Hello("")
+		result := Hello("", "")
 		expected := "Hello, World"
 
 		verifyMessage(t, result, expected)
+	})
+
+	t.Run("Say hello in portuguese", func(t *testing.T) {
+		result := Hello("Breno", "pt-br")
+		expect := "Olá, Breno"
+
+		verifyMessage(t, result, expect)
+	})
+
+	t.Run("Say hello in french", func(t *testing.T) {
+		result := Hello("Breno", "fr")
+		expect := "Bonjour, Breno"
+
+		verifyMessage(t, result, expect)
 	})
 }
