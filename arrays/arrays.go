7104300eef2e31653fb891965f162d9815651b9e
Breno Santos <brenosantos145@gmail.com>
2022-10-31 11:53:44 -0300

    :recycle: change aproach to sum multiples arrays

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -13,17 +13,25 @@ func SumArray(numbers []int) int {
 	return sum
 }
 
-func SumMultiplesArrays(numbers ...[]int) (sums []int) {
-	// Arrays quantity
-	numbersQtd := len(numbers)
-	// make create array with 0 as default value and with x positions
-	sums = make([]int, numbersQtd)
+func SumMultiplesArrays(numbers ...[]int) []int {
+	// // Arrays quantity
+	// numbersQtd := len(numbers)
+	// // make create array with 0 as default value and with x positions
+	// sums = make([]int, numbersQtd)
 
-	// numberArr is each array
-	for i, numberArr := range numbers {
-		// We used the sum array to sum individual array
-		// And save in sums in index position
-		sums[i] = SumArray(numberArr)
+	// // numberArr is each array
+	// for i, numberArr := range numbers {
+	// 	// We used the sum array to sum individual array
+	// 	// And save in sums in index position
+	// 	sums[i] = SumArray(numberArr)
+	// }
+
+	// Declare empty array
+	var sums []int
+
+	for _, numbersArr := range numbers {
+		// append sum to sums array
+		sums = append(sums, SumArray(numbersArr))
 	}
 
 	return sums
