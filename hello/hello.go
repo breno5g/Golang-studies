0843946a782ea4936f7309a89ccf314d22480a29
Breno Santos <brenosantos145@gmail.com>
2022-10-31 10:03:44 -0300

    :recycle: create a helper function to get prefix value

--- hello/hello.go
+++ hello/hello.go
@@ -6,20 +6,28 @@ const prefixInEnglish = "Hello, "
 const prefixInPortuguese = "Olá, "
 const prefixInFrench = "Bonjour, "
 
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := prefixInEnglish
-
+// This is a private function, that's why it starts with a lowercase letter.
+func selectPrefix(language string) (prefix string) {
+	// prefix string create a variavel with start ""
 	switch language {
 	case "pt-br":
 		prefix = prefixInPortuguese
 	case "fr":
 		prefix = prefixInFrench
+	default:
+		prefix = prefixInEnglish
 	}
-	return prefix + name
+	// empty return will return the return typed variable
+	return
+}
+
+// This is a private function, that's why it starts with a lowercase letter.
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return selectPrefix(language) + name
 }
 
 func main() {
