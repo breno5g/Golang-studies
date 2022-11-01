ac2d6b6ac132f1cbb17c8ab710d77ce9e9b97f6e
Breno Santos <brenosantos145@gmail.com>
2022-11-01 08:29:33 -0300

    :sparkles: use interface to abstract the shape and test

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -13,23 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		result := rectangle.Area()
-		expected := 100.0
+	verifyArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		result := shape.Area()
 
 		if result != expected {
 			t.Errorf("result %.2f, expected %.2f", result, expected)
 		}
+	}
+
+	t.Run("Rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		expected := 100.0
+
+		verifyArea(t, rectangle, expected)
 	})
 
 	t.Run("Circle area", func(t *testing.T) {
 		circle := Circle{10}
-		result := circle.Area()
 		expected := 314.1592653589793
 
-		if result != expected {
-			t.Errorf("result %.2f, expected %.2f", result, expected)
-		}
+		verifyArea(t, circle, expected)
 	})
 }
