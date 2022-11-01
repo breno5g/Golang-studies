1388514e394bd9600a1a18ee4ab66b8c02cb3d89
Breno Santos <brenosantos145@gmail.com>
2022-11-01 10:18:17 -0300

    :recycle: add helper function to verify balance test

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -5,18 +5,24 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	verifyBalance := func(t *testing.T, wallet Wallet, expected Bitcoin) {
+		t.Helper()
+
+		result := wallet.Balance()
+
+		if result != expected {
+			t.Errorf("result %s, expected %s", result, expected)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		result := wallet.Balance()
-
 		expected := Bitcoin(10)
 
-		if result != expected {
-			t.Errorf("result %s, expected %s", result, expected)
-		}
+		verifyBalance(t, wallet, expected)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -24,12 +30,8 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(75))
 
-		result := wallet.Balance()
-
 		expected := Bitcoin(75)
 
-		if result != expected {
-			t.Errorf("result %s, expected %s", result, expected)
-		}
+		verifyBalance(t, wallet, expected)
 	})
 }
