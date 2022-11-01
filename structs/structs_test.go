6c350b2622c11c5b5f1d3fe4acf06243906ba822
Breno Santos <brenosantos145@gmail.com>
2022-11-01 08:38:04 -0300

    :recycle: use table driver tests to tests multiples cases

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -12,27 +12,48 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	verifyArea := func(t *testing.T, shape Shape, expected float64) {
-		t.Helper()
-		result := shape.Area()
+// func TestArea(t *testing.T) {
+// 	verifyArea := func(t *testing.T, shape Shape, expected float64) {
+// 		t.Helper()
+// 		result := shape.Area()
 
-		if result != expected {
-			t.Errorf("result %.2f, expected %.2f", result, expected)
-		}
+// 		if result != expected {
+// 			t.Errorf("result %.2f, expected %.2f", result, expected)
+// 		}
+// 	}
+
+// 	t.Run("Rectangle area", func(t *testing.T) {
+// 		rectangle := Rectangle{10.0, 10.0}
+// 		expected := 100.0
+
+// 		verifyArea(t, rectangle, expected)
+// 	})
+
+// 	t.Run("Circle area", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		expected := 314.1592653589793
+
+// 		verifyArea(t, circle, expected)
+// 	})
+// }
+
+func TestArea(t *testing.T) {
+	// Structs array with the format
+	areasToTest := []struct {
+		shape    Shape
+		expected float64
+	}{
+		// Values of the struct array, shape and expected value
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("Rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		expected := 100.0
+	// Get shape of area array
+	for _, tt := range areasToTest {
+		result := tt.shape.Area()
 
-		verifyArea(t, rectangle, expected)
-	})
-
-	t.Run("Circle area", func(t *testing.T) {
-		circle := Circle{10}
-		expected := 314.1592653589793
-
-		verifyArea(t, circle, expected)
-	})
+		if result != tt.expected {
+			t.Errorf("result %.2f, expected %.2f", result, tt.expected)
+		}
+	}
 }
