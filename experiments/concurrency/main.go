a4ffdcf6de52fb9418c495be1d56fb899c44df1e
Breno Santos <brenosantos145@gmail.com>
2023-10-26 20:29:46 -0300

    fix: adding mutex to fix race condition

--- experiments/concurrency/main.go
+++ experiments/concurrency/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 var number uint64 = 0
 
 func main() {
+	m := sync.Mutex{}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		m.Lock()
 		number++
+		m.Unlock()
 		w.Write([]byte(fmt.Sprintf("Hello, world! You are visitor number %d", number)))
 	})
 
