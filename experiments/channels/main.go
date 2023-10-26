2f797368feedff48d8d351b52e0a6f1660ba416b
Breno Santos <brenosantos145@gmail.com>
2023-10-26 20:58:22 -0300

    feat: iterate with range of channels

--- experiments/channels/main.go
+++ experiments/channels/main.go
@@ -4,12 +4,31 @@ import "fmt"
 
 func main() {
 	// First test
-	channel := make(chan string)
+	// channel := make(chan string)
 
-	go func() {
-		channel <- "Hello from another thread"
-	}()
+	// go func() {
+	// channel <- "Hello from another thread"
+	// }()
 
-	msg := <-channel
-	fmt.Println(msg)
+	// msg := <-channel
+	// fmt.Println(msg)
+
+	// Second test
+	ch := make(chan string)
+	go publish(ch)
+	reader(ch)
+}
+
+func reader(ch chan string) {
+	for msg := range ch {
+		fmt.Println(msg)
+	}
+}
+
+func publish(ch chan string) {
+	for i := 0; i < 10; i++ {
+		ch <- fmt.Sprintf("Hello from another thread: %d", i)
+	}
+
+	close(ch)
 }
